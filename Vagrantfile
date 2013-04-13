4736e6eaada55e129afd3a8178a5e7f31bd2c9d1
morgajel <morgajel@gmail.com>
2013-04-13 00:30:13 -0400

    got hiera closer to working

--- Vagrantfile
+++ Vagrantfile
@@ -7,16 +7,18 @@ Vagrant::Config.run do |config|
   config.vm.host_name="shock.vm"
 
   config.vm.box_url = "http://developer.nrel.gov/downloads/vagrant-boxes/CentOS-6.4-x86_64-v20130309.box"
-  config.hiera.config_path = '.'
-  config.hiera.config_file = 'hiera.yaml'
-  config.hiera.data_path   = 'hieradata'
   # config.vm.boot_mode = :gui
 
 
-  config.vm.provision :puppet,  :facter => { "testvalue" => "test" } do |puppet|
+  config.vm.provision :puppet do |puppet|
+    puppet.options = "--hiera_config hiera.yaml"
     puppet.manifests_path = "manifests"
     puppet.manifest_file  = "shock.pp"
     puppet.module_path    = "modules"    
+    puppet.facter = {
+        ## tells default.pp that we're running in Vagrant
+        "is_vagrant" => true,
+    }
   end
 
 end
--- manifests/shock.pp
+++ manifests/shock.pp
@@ -4,22 +4,31 @@ group { "puppet":
   ensure => "present",
 }
 
+#blogs:
+#  - domain:     www.site1.com
+#    dbuser:     site1
+#    dbpassword: funkbunny1
+#    adminuser:  jaxon
+#  - domain:     site2.com
+#    dbuser:     site2
+#    dbpassword: funkbunny2
+#    adminuser:  morgajel
+#
 
-$blogs=hiera('blogs')
-$blogs.each do |blog|
-    blog{$domain: 
-        $domain     => blog['domain'],
-        $adminuser  => blog['adminuser'],
-        $dbuser     => blog['dbuser'],
-        $dbpassword => blog['dbpassword'],
-    }
-end
+#hiera('blogs').each do |$blog|
+#    blog{$domain: 
+#        $domain     => blog['domain'],
+#        $adminuser  => blog['adminuser'],
+#        $dbuser     => blog['dbuser'],
+#        $dbpassword => blog['dbpassword'],
+#    }
+#end
 
 define blog ($domain) {
-  nginx::resource::vhost { "${domain} vhost":
-    ensure   => present,
-    www_root => "/var/www/${domain}/docs/",
-  }
+#  nginx::resource::vhost { "${domain} vhost":
+#    ensure   => present,
+#    www_root => "/var/www/${domain}/docs/",
+#  }
   class { "${domain} wordpress": 
     wp_owner        => $adminuser, 
     wp_group        => 'nginx', 
@@ -29,16 +38,15 @@ define blog ($domain) {
 
 }
 
+$mysqlconfig=hiera('mysql::server')
 class { 'mysql::server':
   config_hash => { 
-    'root_password' =>  hiera('mysql::rootpassword'),
+    'root_password' =>  $mysqlconfig['rootpassword'],
     'port'          =>  '3306',
     'datadir'       =>  '/var/lib/mysql',
     'socket'        =>  '/var/lib/mysql/mysql.sock',
-    'table_cache'   =>  4,
-    'user'          =>  'mysql',
   }
 }
 
 class { 'mysql::php': }
-class { 'nginx': }
+#class { 'nginx': }
